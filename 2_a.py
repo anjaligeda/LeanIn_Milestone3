100c015412da41f5843dae9097c99a0a22392110
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-03-04 11:43:46 +0530

    Create 2_a.py

---
+++ 2_a.py
@@ -0,0 +1,8 @@
+#Check If N and Its Double Exist
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in (arr[:i]+arr[i+1:]):
+                if 2*arr[i] == j:
+                    return True
+        return False
